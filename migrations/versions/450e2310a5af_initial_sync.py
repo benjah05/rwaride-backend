c6407ad24d0340e50873cd838c17f0f64a7754df
Benjah Senzele <senzelebenjamine@gmail.com>
2025-12-18 10:41:28 +0200

    fix: migrations

---
+++ migrations/versions/450e2310a5af_initial_sync.py
@@ -0,0 +1,24 @@
+"""Initial sync
+
+Revision ID: 450e2310a5af
+Revises: 
+Create Date: 2025-12-18 10:37:57.139214
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '450e2310a5af'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
